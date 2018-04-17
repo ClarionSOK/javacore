7ab07f84b5b75489b352f728be5cd92336e1df90
Yuriy Isaev <clarion.isaev@gmail.com>
2018-04-17 16:42:28 +0300

    Circle area programm

---
+++ src/main/java/com/kitcenter/app/homework/lesson6/CircleArea.java
@@ -0,0 +1,7 @@
+package com.kitcenter.app.homework.Homework6;
+
+/**
+ * Created by dell on 17.04.2018.
+ */
+public class CircleArea {
+}
---
+++ src/main/java/com/kitcenter/runners/homework/runners/lesson6/CircleAreaRunner.java
@@ -0,0 +1,7 @@
+package com.kitcenter.runners.homework.runners.lesson6;
+
+/**
+ * Created by dell on 17.04.2018.
+ */
+public class CircleAreaRunner {
+}
